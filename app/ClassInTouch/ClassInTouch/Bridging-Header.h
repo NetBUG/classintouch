0006361a81e307e9217657f67e2a6586e12e4421
Christopher Findeisen <cfindeisen7@gmail.com>
2015-10-24 02:53:00 -0400

    Facebook Login done

---
+++ app/ClassInTouch/ClassInTouch/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  ClassInTouch
+//
+//  Created by Christopher Findeisen on 24/10/2015.
+//  Copyright © 2015 ClassInTouch. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* BridgingHeader_h */
